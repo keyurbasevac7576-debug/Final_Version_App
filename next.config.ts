c2b412d0fd53c5d788b07b38b36f0f1b18411fad
keyurbasevac7576-debug <keyur.basevac7576@gmail.com>
2025-10-22 15:16:10 -0400

    Added tailwind.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.squarespace-cdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.prod.website-files.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
